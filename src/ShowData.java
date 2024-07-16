1971cda6345c2889c06a17db5b4b75f390f2735a
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-07-16 11:27:38 -0400

    Lab complete

---
+++ src/ShowData.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ShowData {
+    public static void main(String[] args) {
+        AddDataToArrayList b = new AddDataToArrayList();
+        ArrayList<Book> mybooklist = b.bookdetails();
+        for(Book showValue : mybooklist){
+            System.out.println(showValue.getNumber() + " " + showValue.getName() + " " + showValue.getCategory() + " " + showValue.getAuthor());
+        }
+    }
+}
